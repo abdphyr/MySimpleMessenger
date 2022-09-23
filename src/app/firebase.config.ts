b925708057e98b5cb4df10b3ab95aa2ff161a2e1
Abdumannon <92175578+abdphyr@users.noreply.github.com>
2022-09-23 20:33:49 +0500

    Create firebase.config.ts

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8joc7PfTr90PVsf69itnA2eR3PDp-mzI",
+  authDomain: "clone-9a497.firebaseapp.com",
+  databaseURL: "https://clone-9a497-default-rtdb.firebaseio.com",
+  projectId: "clone-9a497",
+  storageBucket: "clone-9a497.appspot.com",
+  messagingSenderId: "603503700169",
+  appId: "1:603503700169:web:c706f5b55185ea45514518",
+  measurementId: "G-J6GR3FY8W2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
